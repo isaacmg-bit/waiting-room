3a6146f11f7d2c63c32f8f07923a147bd9ece802
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-02-09 11:21:20 +0100

    chore: created routes and installed mongoose

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Calendar } from './components/calendar/calendar';
+import { Graph } from './components/graph/graph';
+import { Map } from './components/map/map';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'calendar',
+    component: Calendar,
+  },
+  {
+    path: 'graph',
+    component: Graph,
+  },
+  {
+    path: 'map',
+    component: Map,
+  },
+];
