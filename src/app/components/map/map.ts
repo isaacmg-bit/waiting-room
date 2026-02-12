5c235d5f9039afac45f28594eddefc8cf9282c84
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-02-12 12:50:51 +0100

    feat: added Leaflet and geolocation on the map

--- src/app/components/map/map.ts
+++ src/app/components/map/map.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -6,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './map.html',
   styleUrl: './map.css',
 })
-export class Map {
+export class Map implements AfterViewInit {
+  ngAfterViewInit() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const iconDefault = L.icon({
+        iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+        shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+      });
 
+      L.Marker.prototype.options.icon = iconDefault;
+
+      const myLat = position.coords.latitude;
+      const myLng = position.coords.longitude;
+
+      const map = L.map('map', { center: [myLat, myLng], zoom: 13 });
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+      L.marker([myLat, myLng]).addTo(map);
+      setTimeout(() => {
+        map.invalidateSize();
+      }, 0);
+    });
+  }
 }
