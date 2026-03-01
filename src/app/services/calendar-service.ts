f66f6f3f1dce555956db58413c344efa749ff3cf
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-03-01 17:40:44 +0100

    refactor: calendar and calendar service refactor

--- src/app/services/calendar-service.ts
+++ src/app/services/calendar-service.ts
@@ -2,7 +2,6 @@ import { Injectable, inject, signal } from '@angular/core';
 import { UserEvent } from '../models/UserEvent';
 import { ApiService } from './apiservice';
 import { environment } from '../../environments/environment';
-
 @Injectable({
   providedIn: 'root',
 })
@@ -10,58 +9,56 @@ export class CalendarService {
   private readonly api = inject(ApiService);
 
   eventsSignal = signal<UserEvent[]>([]);
-  loadingSignal = signal<boolean>(false);
+  loadingSignal = signal(false);
 
   constructor() {
     this.loadEvents();
   }
 
-  loadEvents(): void {
+  private loadEvents() {
     this.loadingSignal.set(true);
+
     this.api.get<UserEvent[]>(this.getEventsUrl()).subscribe({
       next: (events) => {
         this.eventsSignal.set(events);
         this.loadingSignal.set(false);
       },
       error: (err) => {
-        console.error('Error loading events:', err);
+        console.error(err);
         this.loadingSignal.set(false);
       },
     });
   }
 
-  addEvent(event: UserEvent): void {
+  addEvent(event: UserEvent) {
     this.api.post<UserEvent>(this.getEventsUrl(), event).subscribe({
-      next: () => {
-        this.loadEvents();
+      next: (created) => {
+        this.eventsSignal.update((list) => [...list, created]);
       },
-      error: (err) => console.error('Error adding event:', err),
     });
   }
 
-  deleteEvent(id: string): void {
+  deleteEvent(id: string) {
     const url = `${this.getEventsUrl()}${id}`;
 
     this.api.delete(url).subscribe({
       next: () => {
-        this.loadEvents();
+        this.eventsSignal.update((list) => list.filter((e) => e.id !== id));
       },
-      error: (err) => console.error('Error deleting event:', err),
     });
   }
 
-  editEvent(id: string, body: Partial<UserEvent>): void {
+  editEvent(id: string, body: Partial<UserEvent>) {
     const url = `${this.getEventsUrl()}${id}`;
 
     this.api.patch<UserEvent>(url, body).subscribe({
-      next: () => {
-        this.loadEvents();
+      next: (updated) => {
+        this.eventsSignal.update((list) => list.map((e) => (e.id === updated.id ? updated : e)));
       },
-      error: (err) => console.error('Error updating event:', err),
     });
   }
 
-  private getEventsUrl(): string {
+  private getEventsUrl() {
     return `${environment.apiUrl}${environment.apiEventUrl}`;
   }
 }
