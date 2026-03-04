03da4688267878f3ab5643427c66b76cca4083e0
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-03-04 20:52:43 +0100

    fix: removed the temporary profile logic, as it was overly complicated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { SupabaseService } from './services/supabase-service';
+import { Component } from '@angular/core';
 import { Header } from './components/header/header';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,26 +8,4 @@ import { RouterOutlet } from '@angular/router';
   imports: [Header, RouterOutlet],
   templateUrl: './app.html',
 })
-export class AppComponent implements OnInit {
-  private supabase = inject(SupabaseService);
-
-  ngOnInit() {
-    this.supabase.getClient().auth.onAuthStateChange(async (_, session) => {
-      if (!session?.user) return;
-
-      if (!session.user.email_confirmed_at) return;
-
-      await fetch('http://localhost:3000/users/profile-sync', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${session.access_token}`,
-        },
-        body: JSON.stringify({
-          id: session.user.id,
-          email: session.user.email,
-        }),
-      });
-    });
-  }
-}
+export class AppComponent {}
