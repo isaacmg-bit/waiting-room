2ae21c40ce4a1646af84bf2a3104fb781f830370
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-03-07 22:30:25 +0100

    feat: added profile picture in the header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SupabaseService } from '../../services/supabase-service';
+import { UserService } from '../../services/user-service';
 
 @Component({
   selector: 'app-header',
@@ -10,8 +11,16 @@ import { SupabaseService } from '../../services/supabase-service';
 })
 export class Header {
   private supabase = inject(SupabaseService);
+  private userService = inject(UserService);
+  userId = signal<string | null>(null);
 
   async logout() {
     await this.supabase.signOut();
+  }
+
+  ngOnInit() {
+    this.userService.getMe().subscribe((user) => {
+      this.userId.set(user.id);
+    });
   }
 }
