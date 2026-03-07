020ab0338f55515e5f18cfb63f2ade2c5ad76b21
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-03-07 17:47:51 +0100

    feat: added the same design system for login, register and home components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from './components/header/header';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, Footer],
   templateUrl: './app.html',
 })
 export class AppComponent {}
