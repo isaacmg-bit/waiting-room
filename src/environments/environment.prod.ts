9aa6ab3499ccd621952e9ec6e3e2fb171097f20e
Isaac Malagón <isaacmalagongarcia@gmail.com>
2026-03-25 18:27:04 +0100

    fix: environment data removed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
-// export const environment = {
-//   production: true,
-//   supabaseUrl: '',
-//   supabaseAnonKey: '',
-// };
+export const environment = {
+  production: true,
+  supabaseUrl: '',
+  supabaseAnonKey: '',
+};
